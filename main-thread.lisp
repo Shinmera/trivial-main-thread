fb5e30cd91ce610fb4cb49a5af21c98d47e61a8f
Shinmera <shinmera@tymoon.eu>
2015-06-19 12:42:42 +0200

    Fixed issue on image dump scenario by delegating main thread detection until the latest possible point.

--- main-thread.lisp
+++ main-thread.lisp
@@ -16,7 +16,7 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
    (progn (warn "Couldn't find main thread reliably, choosing last thread.")
           (car (last (bt:all-threads))))))
 
-(defvar *main-thread* (find-main-thread))
+(defvar *main-thread* NIL)
 #+ccl (defvar *housekeeping* NIL)
 
 #+ccl (defun ensure-housekeeping ()
@@ -25,7 +25,11 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
             (setf *housekeeping*
                   (bt:make-thread #'ccl::housekeeping-loop :name "housekeeping"))))
 
-(defun swap-main-thread (new-function &optional (main-thread *main-thread*))
+(defmacro %ensure-mt (var)
+  `(setf ,var (or ,var *main-thread* (find-main-thread))))
+
+(defun swap-main-thread (new-function &optional main-thread)
+  (%ensure-mt main-thread)
   (if (eql (bt:current-thread) main-thread)
       (funcall new-function)
       (let ((new-main (or #+ccl (ensure-housekeeping)
@@ -49,27 +53,31 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
                             (invoke-restart 'simple-tasks:skip))))
       (simple-tasks:start-runner runner))))
 
-(defun start-main-runner (&key (main-thread *main-thread*) (runner *runner*))
+(defun start-main-runner (&key main-thread (runner *runner*))
+  (%ensure-mt main-thread)
   (setf *runner* runner)
   (setf *main-thread* main-thread)
   (swap-main-thread (runner-starter runner) main-thread)
   runner)
 
-(defun stop-main-runner (&key (main-thread *main-thread*) (runner *runner*))
+(defun stop-main-runner (&key main-thread (runner *runner*))
+  (%ensure-mt main-thread)
   (unless (eql (simple-tasks:status runner) :running)
     (error "Main runner ~a already stopped!" runner))
-  (bt:interrupt-thread main-thread (lambda () (invoke-restart 'simple-tasks:abort)))
-  #+ccl (when (and *housekeeping* (bt:thread-alive-p *housekeeping*))
-          (bt:destroy-thread *housekeeping*))
+  (unless (eql (bt:current-thread) main-thread)
+    (bt:interrupt-thread main-thread (lambda () (invoke-restart 'simple-tasks:abort)))
+    #+ccl (when (and *housekeeping* (bt:thread-alive-p *housekeeping*))
+            (bt:destroy-thread *housekeeping*)))
   runner)
 
-(defun ensure-main-runner (&key (main-thread *main-thread*) (runner *runner*))
+(defun ensure-main-runner (&key main-thread (runner *runner*))
+  (%ensure-mt main-thread)
   (unless (eql (simple-tasks:status runner) :running)
     (when (eql (bt:current-thread) main-thread)
       (change-class runner 'simple-tasks:runner))
     (start-main-runner :main-thread main-thread :runner runner)))
 
-(defun ensure-main-runner-started (&key (main-thread *main-thread*) (runner *runner*))
+(defun ensure-main-runner-started (&key main-thread (runner *runner*))
   (ensure-main-runner :main-thread main-thread :runner runner)
   (loop until (eql (simple-tasks:status runner) :running)
         do (sleep 0.01)))
