3ae9041f13f31b9a3a6bbf5b22a47bd5cef6bea3
Baggers <chris.bagley@gmail.com>
2017-06-06 15:04:43 +0200

    use #'sb-thread:main-thread

--- main-thread.lisp
+++ main-thread.lisp
@@ -10,7 +10,7 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
 
 (defun find-main-thread ()
   (or
-   #+sbcl (find "main thread" (bt:all-threads) :from-end T :key #'sb-thread:thread-name :test #'equal)
+   #+sbcl (sb-thread:main-thread)
    #+ecl (find 'si:top-level (bt:all-threads) :from-end T :key #'mp:process-name)
    #+clasp (find 'si:top-level (bt:all-threads) :from-end T :key #'mp:process-name)
    #+ccl ccl::*initial-process*
